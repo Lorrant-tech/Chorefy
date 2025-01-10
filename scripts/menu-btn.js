dca7a4182ac006404893fe3aadc84f5d971233f9
Lorrant Marques <lorrant2000@gmail.com>
2025-01-10 08:08:54 -0300

    Main page: interactivity of menu button and menu visualization finished

--- scripts/menu-btn.js
+++ scripts/menu-btn.js
@@ -1,2 +1,17 @@
-const menuBtnEl = document.querySelector("menu-btn");
+const menuBtnEl = document.querySelector(".menu-btn");
 
+const closeMenuBtnEl = document.querySelector(".close-btn");
+
+const menuContainerEl = document.querySelector(".menu-container");
+
+const overlayEl = document.querySelector(".overlay");
+
+menuBtnEl.addEventListener("click", () => {
+    menuContainerEl.classList.add("menu-active");
+    overlayEl.classList.add("overlay-active");
+})
+
+closeMenuBtnEl.addEventListener("click", () => {
+    menuContainerEl.classList.remove("menu-active");
+    overlayEl.classList.remove("overlay-active");
+})
