306363e9a86fa30f3664ae769892f01b1cf46d3a
Lorrant Marques <lorrant2000@gmail.com>
2025-01-13 11:01:43 -0300

    Main page: renderTask function + OnValue firebase working

--- scripts/index.js
+++ scripts/index.js
@@ -7,30 +7,45 @@ const appSettings = {
 
 const app = initializeApp(appSettings);
 export const database = getDatabase(app);
-export const taskInDB = ref(database, "task");
+export const taskInDB = ref(database, "tasks");
 
 const taskListEl = document.querySelector(".tasks-list");
 
 
-function renderTask(icon, description, deadline) {
+function renderTask(id, icon, description, deadline) {
     const iconEl = document.createElement("div");
     iconEl.classList.add("task-icon");
+    iconEl.id = `icon-${id}`
     iconEl.innerHTML = `
     <i class="${icon}"></i>
     `;
 
     const descriptionEl = document.createElement("div");
-    descriptionEl.classList.add("task-description");
+    descriptionEl.classList.add ("task-description");
+    descriptionEl.id = `description-${id}`
     descriptionEl.innerHTML = description;
 
     const deadlineEl = document.createElement("div");
-    deadlineEl.classList("task-deadline");
+    deadlineEl.classList.add("task-deadline");
+    deadlineEl.id = `deadline-${id}`
     deadlineEl.innerHTML = deadline;
 
-
-
+    taskListEl.appendChild(iconEl);
+    taskListEl.appendChild(descriptionEl);
+    taskListEl.appendChild(deadlineEl);
 }
 
 onValue(taskInDB, function (snapshot) {
-    // console.log(snapshot.val());
+    const tasksListEntries = Object.entries(snapshot.val());
+
+    for (let i = 0; i < tasksListEntries.length; i++) {
+        let currentTask = tasksListEntries[i];
+
+        const currentTaskId = currentTask[0];
+        const currentTaskTasks = currentTask[1];
+        // console.log(currentTaskTasks);
+
+        renderTask(currentTaskId, currentTaskTasks.icon, currentTaskTasks.description, currentTaskTasks.deadline);
+
+    }
 });
