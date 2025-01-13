bd1f589769269ecdeef094ab273559a0a8a0aeb7
Lorrant Marques <lorrant2000@gmail.com>
2025-01-13 14:58:59 -0300

    Main page: done and tbd tasks mechanisms finished.

--- scripts/index.js
+++ scripts/index.js
@@ -11,15 +11,31 @@ export const taskInDB = ref(database, "tasks");
 
 const taskListEl = document.querySelector(".tasks-list");
 
+const checkboxShowDoneEl = document.getElementById("show-done");
 
-function renderTask(id, icon, description, deadline) {
+// const inputContainerEl = document.querySelector(".input-container");
+
+checkboxShowDoneEl.addEventListener("change", renderTaskList);
+
+function renderTask(id, icon, description, deadline, status) {
+    if (checkboxShowDoneEl.checked === true) {
+        createTaskRow(id, icon, description, deadline, status);
+    } else if (checkboxShowDoneEl.checked === false && status === "tbd") {
+        createTaskRow(id, icon, description, deadline, status);
+    }
+}
+
+function createTaskRow(id, icon, description, deadline, status) {
     const iconEl = document.createElement("div");
     iconEl.classList.add("task-icon");
     iconEl.id = `icon${id}`
     iconEl.innerHTML = `
     <i class="${icon}"></i>
     `;
-    iconEl.addEventListener("click", cutDoneTask)
+    iconEl.addEventListener("click", cutDoneTask);
+    if (status === "done") {
+        iconEl.classList.add("highlight")
+    }
 
     const descriptionEl = document.createElement("div");
     descriptionEl.classList.add ("task-description");
@@ -44,20 +60,24 @@ async function cutDoneTask() {
     const taskId = elementId.slice(elementId.indexOf("-"));
 
     const localInDB = ref(database, `tasks/${taskId}`)
+
     const clickedTask = await getTaskinDB(localInDB);
-    if (clickedTask.status === "tbd") {
-        update(localInDB, {"status": "done"})
-    } else {
-        update(localInDB, {"status": "tbd"})
-    }
 
     const rowIconEl = document.getElementById(`icon${taskId}`);
 
-    if (rowIconEl.classList.contains("highlight")) {
-        rowIconEl.classList.remove("highlight");
-    } else {
+    if (clickedTask.status === "tbd") {
+        update(localInDB, {"status": "done"});
         rowIconEl.classList.add("highlight");
+    } else {
+        update(localInDB, {"status": "tbd"});
+        rowIconEl.classList.remove("highlight");
     }
+
+    // if (rowIconEl.classList.contains("highlight")) {
+    //     rowIconEl.classList.remove("highlight");
+    // } else {
+    //     rowIconEl.classList.add("highlight");
+    // }
 }
 
 async function getTaskinDB(pathReference) {
@@ -70,12 +90,18 @@ async function getTaskinDB(pathReference) {
             console.log("Nenhum dado encontrado");
         }
     } catch (error) {
-        console.error("Erro ao obter snapshot: ", error)
+        console.error("Erro ao obter task: ", error)
     }
 }
 
-onValue(taskInDB, function (snapshot) {
+onValue(taskInDB, (snapshot) => {
     const tasksListEntries = Object.entries(snapshot.val());
+
+    taskListEl.innerHTML = `
+        <h2>Icon</h2>
+        <h2>Tasks</h2>
+        <h2>Deadline</h2>
+    `;
 
     for (let i = 0; i < tasksListEntries.length; i++) {
         let currentTask = tasksListEntries[i];
@@ -84,6 +110,35 @@ onValue(taskInDB, function (snapshot) {
         const currentTaskTasks = currentTask[1];
         // console.log(currentTaskTasks);
 
-        renderTask(currentTaskId, currentTaskTasks.icon, currentTaskTasks.description, currentTaskTasks.deadline);
+        renderTask(currentTaskId, currentTaskTasks.icon, currentTaskTasks.description, currentTaskTasks.deadline, currentTaskTasks.status);
     }
 });
+
+async function renderTaskList() {
+    try {
+        const snapshot = await get(taskInDB);
+        if(snapshot.exists()) {
+            const tasksListEntries = Object.entries(snapshot.val());
+            
+            taskListEl.innerHTML = `
+                <h2>Icon</h2>
+                <h2>Tasks</h2>
+                <h2>Deadline</h2>
+            `;
+
+            for (let i = 0; i < tasksListEntries.length; i++) {
+                let currentTask = tasksListEntries[i];
+
+                const currentTaskId = currentTask[0];
+                const currentTaskTasks = currentTask[1];
+                // console.log(currentTaskTasks);
+
+                renderTask(currentTaskId, currentTaskTasks.icon, currentTaskTasks.description, currentTaskTasks.deadline, currentTaskTasks.status);
+            }
+        } else {
+            console.log("Nenhum dado encontrado");
+        }
+    } catch (error) {
+        console.error("Erro ao obter snapshot: ", error);
+    }
+}
