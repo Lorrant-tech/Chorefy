4f71b42ead1f9e819a80efec72c61f9891955972
Lorrant Marques <lorrant2000@gmail.com>
2025-01-13 19:27:32 -0300

    Refactoring: Menu styles changed to their own file (style.css -> menu.css). Favicon html adjustments. Hard coded numbers (mocks) in HTML excluded. Minor clean ups and better CSS organization

--- scripts/task-stats.js
+++
@@ -1,8 +0,0 @@
-const assignedTasksEl = document.getElementById("assigned-tasks");
-const completedTasksEl = document.getElementById("completed-tasks");
-const remainingTasksEl = document.getElementById("remaining-tasks");
-
-// Substitute values using firebase later
-assignedTasksEl.innerText = 12;
-completedTasksEl.innerText = 11;
-remainingTasksEl.innerText = assignedTasksEl.innerText - completedTasksEl.innerText;
